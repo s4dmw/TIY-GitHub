cb67354009178ccbc5d5ef1da84f9be5046f6e1c
David <dmwhite@gmail.com>
2015-09-29 20:02:29 -0400

    accessed octocat.json with ajax

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+(function(){
+  // console.log("IIFE working!") //tracer bullet
+  // $.ajax({url: "https://api.github.com/users/octocat"});
+  // console.log($.ajax({url:"../apis/github/users/octocat.json"}));
+  console.log($.ajax({url:"../apis/github/users/octocat.json"}));
+})();
