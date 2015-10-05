895aa2d51e2562d9dec08b1197f3e77660ac210c
David <dmwhite@gmail.com>
2015-10-05 15:38:53 -0400

    house cleaning

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,16 @@
   // $.ajax({url: "https://api.github.com/users/octocat"});
   // console.log($.ajax({url:"../apis/github/users/octocat.json"}));
   console.log($.ajax("../apis/github/users/octocat.json"));
+  $.ajax("../apis/github/users/octocat.json").then(function(){
+    console.log(arguments)
+  });
+  var email;
+  console.log("first email: " + email);
+  $.ajax("../apis/github/users/octocat.json").then(function(){
+    console.log(arguments[0].email);
+    email = arguments[0].email;
+    console.log("email: " + email);
+  });
+  console.log("outside the function - email: " + email);
 
 })();
