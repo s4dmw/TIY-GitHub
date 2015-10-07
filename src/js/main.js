3c57af5a25408979ee63478747a5b46e0bcb6c48
David <dmwhite@gmail.com>
2015-10-07 00:15:45 -0400

    place holders filled

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,7 @@
     $("nav#tabs h3").removeClass("active");
     $(this).addClass("active");
     $elementId = $("h3.active").get(0).id;
-    console.log($elementId);
+    // console.log($elementId);
     $("section.panel").addClass("inactive");
     $("section#"+$elementId).removeClass("inactive");
   });
@@ -38,9 +38,20 @@
     $("a:contains('email')").html(info.email).attr("href", "mailto:"+info.email);
     // console.log(new Date(info.created_at));
     $("li:contains('created_at')").html(new Date(info.created_at));
+    $("strong:contains('followers')").html(info.followers);
+    $("strong:contains('following')").html(info.following);
+  }); //end user data section
 
-  });
-
-
+//not the right way to do this but just making something happen so i can go to bed!!
+  $.getJSON("../apis/github/users/s4dmw/repositories.json", function(){
+    repos = arguments[0][0];
+    console.log(repos);
+    $("li :contains('name')").html(repos.name);
+    $("li :contains('description')").html(repos.description);
+    $("li :contains('updated_at')").html(repos.updated_at);
+    $("li :contains('language')").html(repos.language);
+    $("li :contains('stargazers_count')").html(repos.stargazers_count);
+    $("li :contains('forks_count')").html(repos.forks_count);
+  }); //end repos section
 
 })();
